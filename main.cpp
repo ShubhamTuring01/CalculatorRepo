28d6fbd95caa76147f82d6efd834c196dcd0e9a1
ShubhamTuring01 <shubham.g@turing.com>
2025-03-12 11:40:39 +0530

    Basic main code

--- main.cpp
+++ main.cpp
@@ -3,5 +3,22 @@
 
 int main()
 {
+	int firstNum=0,secondNum=0;
+	char op = ' ';
+
+	std::cout << "Welcome to basic calculator\n";
+
+	std::cout << "Enter first number:"<<"\n";
+	std::cin >> firstNum;
+	
+	std::cout << "Select the opration:"<<"\n";
+	std::cout << "+ for add\n";
+	std::cin >> op;
+	std::cout << "Enter second num\n";
+	std::cin >> secondNum;
+
+	long long result;
+
+	
 	return 0;
 }
