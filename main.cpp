49ab74f9ec92396754d2eb6103bbfc96d3298301
ShubhamTuring01 <shubham.g@turing.com>
2025-03-12 11:47:29 +0530

    Add function added in calculator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 //Project Simple Calculator
 #include <iostream>
 
+
+long long add(int firstNum, int secondNum)
+{
+	return firstNum + secondNum;
+}
+
 int main()
 {
 	int firstNum=0,secondNum=0;
@@ -17,8 +23,12 @@ int main()
 	std::cout << "Enter second num\n";
 	std::cin >> secondNum;
 
-	long long result;
+	long long result=0;
 
+	if (op == '+')
+		result += add(firstNum, secondNum);
+	
+	std::cout << "\nResult:" << result << "\n";
 	
 	return 0;
 }
